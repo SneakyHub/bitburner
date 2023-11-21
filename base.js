51d793f3e2c2b9d03ad419e9ef0c65703b8b0562
Sneaky <78713939+SneakyHub@users.noreply.github.com>
2023-11-21 19:58:24 +0000

    Create base.js

---
+++ base.js
@@ -0,0 +1,10 @@
+export async function main(ns) {
+    while(true) {
+    await ns.weaken('iron-gym');
+    await ns.hack('iron-gym');
+    await ns.hack('iron-gym');
+    await ns.hack('iron-gym');
+    await ns.hack('iron-gym');
+    await ns.hack('iron-gym');
+    }
+}
