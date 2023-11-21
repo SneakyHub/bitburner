14b07bfda6671d697077e477a37f011d142915a5
Sneaky <78713939+SneakyHub@users.noreply.github.com>
2023-11-21 22:10:27 +0000

    Update base.js

--- base.js
+++ base.js
@@ -1,10 +1,13 @@
+var target = "phantasy"; // put your victim here
+
 export async function main(ns) {
+
     while(true) {
-    await ns.weaken('iron-gym');
-    await ns.hack('iron-gym');
-    await ns.hack('iron-gym');
-    await ns.hack('iron-gym');
-    await ns.hack('iron-gym');
-    await ns.hack('iron-gym');
+    await ns.weaken(target);
+    await ns.hack(target);
+    await ns.hack(target);
+    await ns.hack(target);
+    await ns.hack(target);
+    await ns.hack(target);
     }
 }
