6f940a12236c9f153198daeb5dfdfc2b9edf138c
Sneaky <78713939+SneakyHub@users.noreply.github.com>
2023-11-21 19:51:49 +0000

    Create base.js

---
+++ base.js
@@ -0,0 +1,10 @@
+cp base.js base1.js
+cp base.js base2.js
+cp base.js base3.js
+cp base.js base4.js
+cp base.js base5.js
+cp base.js base6.js
+cp base.js base7.js
+cp base.js base8.js
+cp base.js base9.js
+cp base.js base10.js
